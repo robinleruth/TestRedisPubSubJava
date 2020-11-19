3445b8a7722e50bfbd9e4fe8f80b500e8d2a9408
robinleruth <robin.leruth@gmail.com>
2020-11-19 13:40:30 +0100

    first commit

---
+++ Subscriber/src/test/java/org/robin/testpubsub/Subscriber/SubscriberApplicationTests.java
@@ -0,0 +1,13 @@
+package org.robin.testpubsub.Subscriber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ setup-host.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+# This script installs all the requireed dependencies on a host, which is
+# assumed to be Ubuntu 16.04.
+
+# install system dependencies
+apt-get update -y
+apt-get install -y maven default-jdk
+
+# install docker
+curl -fsSL https://get.docker.com/ | sh
+usermod -aG docker vagrant
+
+apt-get remove docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
